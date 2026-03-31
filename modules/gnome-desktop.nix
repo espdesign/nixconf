997b1dfe7609ecc91df22a7fce6a8d1ffffcef5a
espdesign <evanpendergraft@gmail.com>
2026-03-31 14:53:13 -0400

    add wallpaper and custom gnome settings

--- modules/gnome-desktop.nix
+++ modules/gnome-desktop.nix
@@ -1,38 +1,68 @@
-{ den, lib, pkgs, ... }:
+{
+  den,
+  lib,
+  pkgs,
+  ...
+}:
 {
   den.aspects.gnome-desktop = {
-    nixos = { pkgs, ... }: {
-      services.displayManager.gdm.enable = true;
-      services.desktopManager.gnome.enable = true;
-    };
+    nixos =
+      { pkgs, ... }:
+      {
+        services.displayManager.gdm.enable = true;
+        services.desktopManager.gnome.enable = true;
 
-    darwin = { pkgs, ... }: { };
-
-    homeManager = { pkgs, ... }: {
-      home.packages = with pkgs; [
-        gnome.gnome-tweaks
-        gnome.gnome-system-monitor
-        gnome.adwaita-icon-theme
-      ];
-
-      dconf.settings = {
-        "/org/gnome/desktop/interface" = {
-          color-scheme = "prefer-dark";
-          gtk-theme = "adwaita-dark";
-          icon-theme = "Adwaita";
-        };
-        "/org/gnome/desktop/peripherals/mouse" = {
-          natural-scroll = true;
-        };
-      };
-    };
-
-    provides.extensions = {
-      homeManager = { pkgs, ... }: {
-        home.packages = with pkgs; [
-          gnomeExtensions.gsconnect
+        environment.systemPackages = with pkgs; [
+          gnomeExtensions.appindicator
         ];
       };
-    };
+
+    # darwin = { pkgs, ... }: { };
+
+    homeManager =
+      { pkgs, config, ... }:
+      {
+        home.file.".local/share/backgrounds/wallpaper-molly.jpg".source = ./static/wallpaper-molly.jpg;
+
+        # 2. Configure GNOME Settings
+        dconf = {
+          enable = true;
+          settings = {
+            "org/gnome/shell" = {
+              disable-user-extensions = false;
+              # Enable the app indicator extension (tray icons)
+              enabled-extensions = [ "appindicatorsupport@rgcjonas.gmail.com" ];
+              favorite-apps = [
+                "org.gnome.Nautilus.desktop"
+                "firefox.desktop"
+                "com.mitchellh.ghostty.desktop"
+              ];
+            };
+
+            "org/gnome/desktop/interface" = {
+              show-battery-percentage = true;
+              clock-format = "12h";
+              color-scheme = "prefer-dark";
+            };
+
+            # Set desktop background for dark and light mode
+            "org/gnome/desktop/background" = {
+              picture-uri = "file://${config.home.homeDirectory}/.local/share/backgrounds/wallpaper-molly.jpg";
+              picture-uri-dark = "file://${config.home.homeDirectory}/.local/share/backgrounds/wallpaper-molly.jpg";
+            };
+          };
+        };
+
+      };
+
+    # provides.extensions = {
+    #   homeManager =
+    #     { pkgs, ... }:
+    #     {
+    #       home.packages = with pkgs; [
+    #         gnomeExtensions.gsconnect
+    #       ];
+    #     };
+    # };
   };
 }
